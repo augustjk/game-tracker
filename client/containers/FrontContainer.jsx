aae1e57129f3c3ac8b19d7cf20bd46d2e6d0115b
Sterling Deng <sterlingdeng@gmail.com>
2019-03-09 19:11:08 -0800

    used material boot strap for name input box. prettier reformatted code

--- client/containers/FrontContainer.jsx
+++ client/containers/FrontContainer.jsx
@@ -16,8 +16,8 @@ class FrontContainer extends Component {
       p2Score: [0],
       serving: [],
       maxScore: 21,
-      endGameChoice: null,
-    }
+      endGameChoice: null
+    };
     this.setP1Name = this.setP1Name.bind(this);
     this.setP2Name = this.setP2Name.bind(this);
     this.startMatch = this.startMatch.bind(this);
@@ -26,21 +26,22 @@ class FrontContainer extends Component {
     this.handleScoreButton = this.handleScoreButton.bind(this);
     this.handleEndGame = this.handleEndGame.bind(this);
   }
-  
+
   componentDidMount() {
-    axios.post('/action', {action: 'GET_STATE'})
-    .then(res=>{
-      this.setState(res.data);
-    })
-    .catch(err=>console.error(err));
+    axios
+      .post('/action', { action: 'GET_STATE' })
+      .then(res => {
+        this.setState(res.data);
+      })
+      .catch(err => console.error(err));
   }
 
   setP1Name(e) {
-    this.setState({p1name: e.target.value});
+    this.setState({ p1name: e.target.value });
   }
 
   setP2Name(e) {
-    this.setState({p2name: e.target.value});
+    this.setState({ p2name: e.target.value });
   }
 
   setServing(e) {
@@ -50,7 +51,7 @@ class FrontContainer extends Component {
     } else if (e.target.id === 'p2-serve') {
       serving = 2;
     }
-    this.setState({serving});
+    this.setState({ serving });
   }
 
   setMaxScore(e) {
@@ -60,7 +61,7 @@ class FrontContainer extends Component {
     } else if (e.target.id === '11-point') {
       maxScore = 11;
     }
-    this.setState({maxScore});
+    this.setState({ maxScore });
   }
 
   startMatch() {
@@ -70,44 +71,46 @@ class FrontContainer extends Component {
         p1name: this.state.p1name,
         p2name: this.state.p2name,
         maxScore: this.state.maxScore,
-        serving: this.state.serving,
+        serving: this.state.serving
       }
-    }
-    axios.post('/action', body)
-    .then(res=>{
-      if (res.status === 200) {
-        this.setState(res.data);
-      }
-    })
-    .catch(err=>console.error(err));
+    };
+    axios
+      .post('/action', body)
+      .then(res => {
+        if (res.status === 200) {
+          this.setState(res.data);
+        }
+      })
+      .catch(err => console.error(err));
   }
 
   handleScoreButton(e) {
     const body = {};
-    switch(e.target.id) {
+    switch (e.target.id) {
       case 'inc-p1':
         body.action = 'INCREMENT';
-        body.payload = {player: 1};
+        body.payload = { player: 1 };
         break;
       case 'inc-p2':
         body.action = 'INCREMENT';
-        body.payload = {player: 2};
+        body.payload = { player: 2 };
         break;
       case 'undo':
         body.action = 'UNDO';
         break;
     }
 
-    axios.post('/action', body)
-    .then(res=>{
-      this.setState(res.data);
-    })
-    .catch(err=>console.error(err));
+    axios
+      .post('/action', body)
+      .then(res => {
+        this.setState(res.data);
+      })
+      .catch(err => console.error(err));
   }
 
   handleEndGame(e) {
-    const body = {action: 'RESTART'};
-    switch(e.target.id) {
+    const body = { action: 'RESTART' };
+    switch (e.target.id) {
       case 'rematch':
         body.payload = 2;
         break;
@@ -119,44 +122,51 @@ class FrontContainer extends Component {
         break;
     }
 
-    axios.post('/action', body)
-    .then(res=>{
-      this.setState(res.data);
-    })
-    .catch(err=>console.error(err));
+    axios
+      .post('/action', body)
+      .then(res => {
+        this.setState(res.data);
+      })
+      .catch(err => console.error(err));
   }
 
   render() {
     switch (this.state.gameState) {
       case 0:
-        return (<NoGameContainer
-          p1name={this.state.p1name}
-          p2name={this.state.p2name}
-          serving={this.state.serving}
-          maxScore={this.state.maxScore}
-          setP1Name={this.setP1Name}
-          setP2Name={this.setP2Name}
-          startMatch={this.startMatch}
-          setMaxScore={this.setMaxScore}
-          setServing={this.setServing}
-          />) ;
+        return (
+          <NoGameContainer
+            p1name={this.state.p1name}
+            p2name={this.state.p2name}
+            serving={this.state.serving}
+            maxScore={this.state.maxScore}
+            setP1Name={this.setP1Name}
+            setP2Name={this.setP2Name}
+            startMatch={this.startMatch}
+            setMaxScore={this.setMaxScore}
+            setServing={this.setServing}
+          />
+        );
       case 1:
-       return (<PlayingContainer
-          p1name={this.state.p1name}
-          p2name={this.state.p2name}
-          serving={this.state.serving}
-          p1Score={this.state.p1Score}
-          p2Score={this.state.p2Score}
-          handleScoreButton={this.handleScoreButton}
-         />);
+        return (
+          <PlayingContainer
+            p1name={this.state.p1name}
+            p2name={this.state.p2name}
+            serving={this.state.serving}
+            p1Score={this.state.p1Score}
+            p2Score={this.state.p2Score}
+            handleScoreButton={this.handleScoreButton}
+          />
+        );
       case 2:
-        return (<EndGameContainer
-          p1name={this.state.p1name}
-          p2name={this.state.p2name}
-          p1Score={this.state.p1Score}
-          p2Score={this.state.p2Score}
-          handleEndGame={this.handleEndGame}
-        />);
+        return (
+          <EndGameContainer
+            p1name={this.state.p1name}
+            p2name={this.state.p2name}
+            p1Score={this.state.p1Score}
+            p2Score={this.state.p2Score}
+            handleEndGame={this.handleEndGame}
+          />
+        );
       default:
         return null;
     }
