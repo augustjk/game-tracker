d27f40449b57f4dd8ab768c8d9ddf277a1268bbc
Augustine Kim <ajk830@gmail.com>
2019-03-09 18:08:29 -0800

    webpack config change to add dev-servery proxy for /sidebar

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     contentBase: path.resolve(__dirname, './client'),
     publicPath: '/build/',
     proxy: {
-      '/action': 'http://localhost:3000'
+      '/action': 'http://localhost:3000',
+      '/sidebar': 'http://localhost:3000'
     }
   },
   output: {
